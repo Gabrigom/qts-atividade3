27652556b48d93f9026ac1eff91b7ea64b514fb3
Gabrigom <vinny.gab18@gmail.com>
2025-11-23 15:45:04 -0300

    chore: linting

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ from typing import Optional
 
 app = FastAPI(title="API TDD VGMG")
 
+
 class Item(BaseModel):
     item_id: int
     name: str
     description: Optional[str] = None
     price: float
+
 
 mock_db = {
     1: {"item_id": 1, "name": "Item 1"},
@@ -16,13 +18,15 @@ mock_db = {
     3: {"item_id": 3, "name": "Item 3"},
 }
 
+
 @app.get("/")
 def ler_root():
     return {"message": "API TDD VGMG funcionando!"}
-  
+
+
 @app.get("/items/{item_id}")
 def get_item(item_id: int):
     if item_id not in mock_db:
         raise HTTPException(status_code=404, detail="Cartão Inválido")
-      
+
     return mock_db[item_id]
--- tests/test_items.py
+++ tests/test_items.py
@@ -3,11 +3,13 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_get_item_sucesso():
     response = client.get("/items/1")
     assert response.status_code == 200
     assert response.json() == {"item_id": 1, "name": "Item 1"}
-    
+
+
 def test_get_item_nao_encontrado():
     response = client.get("/items/999")
     assert response.status_code == 404
